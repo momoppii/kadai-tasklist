24db13c997193798d7730e7ba304274316f6b0a3
momoka.kai <momoppiiiii0718@gmail.com>
2021-04-25 16:20:56 +0000

    fixed-0426

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,7 +51,7 @@ class TasksController < ApplicationController
         @task.destroy
         
         flash[:success] = 'Task は削除されました'
-        redirect_to task_url
+        redirect_to tasks_url
     end
     
     def task_params
