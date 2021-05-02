cb02cc4a774e2cdf7efd3d6e247c27342a80821b
momoka.kai <momoppiiiii0718@gmail.com>
2021-05-02 17:56:33 +0000

    toppage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-    root to: 'tasks#index'
+  get 'toppages/index'
+    root to: 'toppages#index'
     
-    resources :tasks
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+    
+    resources :tasks, only: [:index, :show, :create]
   end
